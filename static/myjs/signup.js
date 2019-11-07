181271846f3c908eef4f1b2a7ee09226b8aa6ddd
Harmanpreet904 <harmanpreet904@gmail.com>
2019-11-07 22:58:54 +0530

    new changed

--- static/myjs/signup.js
+++ static/myjs/signup.js
@@ -300,5 +300,12 @@ function userlogin() {
     }
 }
 
-
+function showpass() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
